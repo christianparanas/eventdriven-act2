deeafce3ddcdd186abce028c72471456e5aac0c1
christianparanas <christiannparanas@gmail.com>
2021-09-24 22:22:31 +0800

    :sparkles: init nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
+  onScroll: boolean = false
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    window.addEventListener("scroll", this.listenScrollEvent)
   }
 
+  listenScrollEvent = () => {
+    window.scrollY > 15 ? this.onScroll = true : this.onScroll = false;
+  }
 }
