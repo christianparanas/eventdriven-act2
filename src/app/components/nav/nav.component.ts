d60b760e30fe64e9cd0ebdee9e97bb828eff452d
christianparanas <christiannparanas@gmail.com>
2021-09-26 00:42:05 +0800

    :sparkles: added nav overlay

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,25 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
+  animations: [
+    trigger('inOutAnimation', [
+      transition(':enter', [
+        style({ top: '-100px', opacity: 1 }),
+        animate('.3s ease', style({ top: 0, opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ top: 0, opacity: 1 }),
+        animate('.3s ease', style({ top: '-1000px', opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class NavComponent implements OnInit {
-  onScroll: boolean = false
-  isOpenNavOverlay: boolean = false
+  onScroll: boolean = false;
+  isOpenNavOverlay: boolean = false;
 
   constructor() {}
 
   ngOnInit(): void {
-    window.addEventListener("scroll", this.listenScrollEvent)
+    window.addEventListener('scroll', this.listenScrollEvent);
   }
 
   openCloseNavOverlay(): void {
-    this.isOpenNavOverlay =! this.isOpenNavOverlay
+    this.isOpenNavOverlay = !this.isOpenNavOverlay;
   }
 
   listenScrollEvent = () => {
-    window.scrollY > 15 ? this.onScroll = true : this.onScroll = false;
-  }
+    window.scrollY > 15 ? (this.onScroll = true) : (this.onScroll = false);
+  };
 }
