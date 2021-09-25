b1899d213d63690484b32b0fc0aa152a1a356f74
christianparanas <christiannparanas@gmail.com>
2021-09-25 23:07:25 +0800

    :fire: revert to single page portfolio and added hero section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { GalleryComponent } from './pages/gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     FooterComponent,
-    HomeComponent,
-    GalleryComponent
   ],
   imports: [
     BrowserModule,
